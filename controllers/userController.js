df9ccb4fa2f3a7a55d7c44eaca898e7c83375783
Paul Otieno <vitalispaul48@live.com>
2023-09-20 04:50:55 +0300

    Log in user

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,7 @@
 const asyncHandler = require("express-async-handler");
 const User = require("../models/userModel");
 const bcrypt =  require("bcrypt")
-
+const jwt = require("jsonwebtoken")
 //@desc Register a user
 //@route POST /api/users/register
 //@access public
@@ -44,7 +44,31 @@ const registerUser = asyncHandler( async(req, res) => {
 //@access public
 
 const loginUser = asyncHandler( async(req, res) => {
-    res.json({message: "Login user"})
+    const {email, password} = req.body;
+    if(!email || !password) {
+        res.status(400);
+        throw new Error("All fields are mandatory!")
+    }
+
+    const user = await User.findOne({email})
+
+    // compare password with hashedpasswordd
+    if(user && (await bcrypt.compare(password, user.password))) {
+        const accessToken = jwt.sign({
+            user: {
+                username: user.username,
+                email: user.email,
+                id: user.id
+            }
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {expiresIn: "1m"}
+        )
+        res.status(200).json({accessToken})
+    } else {
+        res.status(401)
+        throw new Error("email or password is not valid")
+    }
 });
 
 //@desc Current user info
